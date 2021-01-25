5e8e6f3dbeb489dc277f795a06d7cc7d7c26f273
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-01-25 09:10:56 -0500

    Added id to all textarea tags

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,36 @@
+
+function checkTime() {
+    var time = moment().format("hA");
+    var timeInt = parseInt(time);
+    if (timeInt < 9) {
+        $("#9:00").addClass("past");
+    } else if (timeInt === 9) {
+        $("#9:00").addClass("present");
+    } else {
+        $("#9:00").addClass("future");
+    }
+
+};
+
+
+checkTime();
+
+
 var time = moment().format("hA");
 
+var timeInt = parseInt(time);
+
+console.log(timeInt);
 console.log(time);
 
+//thursday, january 25th
+moment().format("dddd, MMMM, Do")
 
-$("#9amTextArea").addClass(".past")
+
+$(".saveBtn").on('click', function() {
+
+    var text = $(".text").val();
+    localStorage.setItem(text, value);
+    var text = $(this).parent().attr("id");
+
+}) 
